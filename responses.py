afcecb86161156e325a213c6330b60114e0d6e9a
Aaron Ng <aaronng139@gmail.com>
2023-05-28 16:01:25 -0400

    added randint command

--- responses.py
+++ responses.py
@@ -1,10 +1,25 @@
+import random
+
 def handle_response(msg) -> str:
     text = msg.lower()
     
-    if text == 'hello':
-        return 'hello world!'
+    if text == 'ahoy':
+        return 'ahoy!'
        
     if text == "help":
-        return "`help message`"
+        return """
+        `Bot Commands: \n(rand #min #max) Outputs random integer between #min (inclusive) and #max (exclusive)`
+        """
     
-    return "default response"
+    # Rolling a random integer
+    if text[:5] == "rand ":
+        text = text[5:].split()
+        if len(text) > 2:
+            return "Error: rand only takes 2 numerical arguments (rand #min #max)"
+        if not (text[0].isdigit() and text[1].isdigit()):
+            return "Error: rand needs 2 integers (rand #min #max)"
+        num_min = int(text[0])
+        num_max = int(text[1])
+        if num_min > num_max:
+            return "Error: max cannot be less than min (rand #min #max)"
+        return random.randint(num_min, num_max)
